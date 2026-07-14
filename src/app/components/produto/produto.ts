ae8581073b9478072eb2b6f261e2d69aa7fe73bf
Enzo <gabrielcardosorodriguez@gmail.com>
2026-07-13 21:56:35 -0300

    feat: modifica o codigo adicionando 3 produtos e um import do UpperCasePipe

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import{UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
-export class Produto {}
+// adicionando a classe Produto com as propriedades produto, preco, e variavél.
+export class Produto {
+  produto = 'Notebook Gamer';
+  preco = 5000;
+  mostrarProduto = true;
+  mostrarPreco = true;
+  produtos =[
+    {nome: 'Teclado', preco:49.99},
+    {nome: 'Mouse', preco:29.99},
+    {nome: 'Monitor', preco:159.99}
+  ];
+}
