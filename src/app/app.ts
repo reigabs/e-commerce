1b6fe23038287f4abe7eee68fa71b13f053e4513
Enzo <gabrielcardosorodriguez@gmail.com>
2026-07-17 19:10:39 -0300

    feat: importa as variaveis em nossa moeda, depois adicionamos uma saida de dados para os produtos selecionados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router'; //remove a importação de RouterOutlet , pois não é necessaria para esse componente
-import { Produto } from './components/produto/produto'; // importando a classe Produto do arquivo produto.ts do componente app
+//import { Produto } from './components/produto/produto'; // importando a classe Produto do arquivo produto.ts do componente app
+  import {ListaProdutos} from './features/produtos/lista-produtos/lista-produtos'; 
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
