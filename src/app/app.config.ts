3429dc8b7089795217f0bfd7c76b2cea363ce58a
gabriel <gabrielcardosorodriguez@gmail.com>
2026-07-24 21:30:56 -0300

    style: organizar o codigo dos arquivos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
+    provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()),
   ]
 };
