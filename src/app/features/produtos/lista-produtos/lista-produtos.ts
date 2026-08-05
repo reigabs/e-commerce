32f3fc263546c276557638a921581af34850700e
Gabriel Cardoso <gabrielcardosorodriguez@gmail.com>
2026-08-04 21:19:00 -0300

    style: tira UpercasePiper nos importes no componente

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -10,7 +10,7 @@ import { produtosService } from '../produtos.service';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto, PrecoFormatadoPipe, UpperCasePipe],
+  imports: [Produto, PrecoFormatadoPipe,],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
