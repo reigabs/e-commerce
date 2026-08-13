45816ad52a8c4dcf0e52aee366e548340de6f9b8
Gabriel Cardoso <gabrielcardosorodriguez@gmail.com>
2026-08-13 19:42:54 -0300

    feat(http): integra interceptor ao token JWT

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -1,34 +1,37 @@
+import { inject } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
-import { tap } from "rxjs"
-import { catchError } from "rxjs";
-import { throwError } from "rxjs";
+import { tap, catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
-    console.log('Interceptando requisição:', req.body)
-    const token = 'fake-token-jwt';
-    const novaReq = req.clone({
-        setHeaders: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return next(novaReq).pipe(
-        tap({
-            next: (event) => console.log('Responde: ', event),
-            error: (error) => console.error('Erro de Requisição: ', error),
-        }),
-
-        catchError((error) => {
-            console.error('ERRO GLOBAL: ', error);
-            if (error.status === 401) {
-                console.warn('Usuario não autorizado!');
-            }
-
-            if (error.status === 500) {
-                console.warn('Erro interno do servidor!')
-            }
-            
-            return throwError(() => error);
-        })
-    );
+const authService = inject(AuthService);
+const token = authService.obterToken();
+// LOG REQUEST
+console.log('REQUEST', req.url);
+// TOKEN
+const novaReq = token
+? req.clone({
+setHeaders: {
+Authorization: `Bearer ${token}`,
+},
+})
+: req;
+// SEGUE COM A NOVA REQUEST + LOG RESPONSE
+return next(novaReq).pipe(
+tap({
+next: (event) => console.log('RESPONSE:', event),
+error: (error) => console.error('ERRO:', error),
+}),
+catchError((error) => {
+console.error('ERRO GLOBAL:', error);
+if (error.status === 401) {
+console.warn('Não autorizado!');
+}
+if (error.status === 500) {
+console.warn('Erro interno do servidor!');
+}
+return throwError(() => error);
+}),
+);
 
 };
