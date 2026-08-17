e5b31c8141aec5dad5bb641aeae1870737fe9607
gabriel cardoso <gabrielcardosorodriguez@gmail.com>
2026-08-17 19:33:19 -0300

    style: diferencia usuario comum de admin

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,10 +1,8 @@
 import { Injectable, computed, signal } from '@angular/core';
-
-
-
+type PerfilUsuario = 'usuario' | 'admin';
 type Usuario = {
 email: string;
-perfil: 'usuario';
+perfil: PerfilUsuario;
 };
 @Injectable({
 providedIn: 'root',
@@ -14,18 +12,21 @@ private usuario = signal<Usuario | null>(null);
 private tokenJwt = signal<string | null>(null);
 usuarioAtual = computed(() => this.usuario());
 estaLogado = computed(() => this.usuario() !== null);
+ehAdmin = computed(() => this.usuario()?.perfil === 'admin');
 token = computed(() => this.tokenJwt());
 login(email: string, senha: string): boolean {
 if (!email || !senha) {
 return false;
 }
+// Regra: e-mail perfil será admin: admin@email.com . Outro e-mail: usuario comum.
+const perfil: PerfilUsuario = email === 'admin@email.com' ? 'admin' : 'usuario';
 const tokenSimulado =
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
 'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' +
 'assinatura-simulada';
 this.usuario.set({
 email,
-perfil: 'usuario',
+perfil,
 });
 this.tokenJwt.set(tokenSimulado);
 return true;
@@ -36,5 +37,8 @@ this.tokenJwt.set(null);
 }
 obterToken(): string | null {
 return this.tokenJwt();
+}
+obterPerfil(): PerfilUsuario | null {
+return this.usuario()?.perfil ?? null;
 }
 }
