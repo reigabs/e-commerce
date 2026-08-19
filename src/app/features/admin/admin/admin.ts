11545c481f9b0ed923b70949db82b3947146b320
gabriel cardoso <gabrielcardosorodriguez@gmail.com>
2026-08-19 19:24:57 -0300

    feat(admin): Transforma a tela admin em uma área mais realista e funcional.

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,4 +8,28 @@ import { Component } from '@angular/core';
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
-export class Admin {}
+export class Admin {
+  private authService = inject(AuthService);
+private router = inject(Router);
+
+totalProdutosCadastrados = signal (20);
+pedidosPendentes = signal (3);
+usuariosCadastrados = signal (8);
+
+usuarioAtual = this.authService.usuarioAtual;
+
+mensagemPerfil = computed(() => {
+const usuario = this.usuarioAtual();
+
+if (!usuario) {
+return 'Nenhum usuário autenticado.';
+}
+
+return `Usuário autenticado como ${usuario.perfil}.`;
+});
+sair () {
+  this.authService.logout();
+this.router.navigateByUrl('/login');
+}
+
+}
