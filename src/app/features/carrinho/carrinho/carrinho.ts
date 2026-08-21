2f50c6b4e53f9d988d86a721a551eedbb6293568
gabriel cardoso <gabrielcardosorodriguez@gmail.com>
2026-08-21 19:13:31 -0300

    feat: implementar página de revisão do carrinho

--- src/app/features/carrinho/carrinho/carrinho.ts
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -1,24 +1,12 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { CarrinhoFacade } from '../../../core/facades/carrinho.facades';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carrinho',
-  imports: [RouterLink, MatButtonModule],
+  imports: [],
   templateUrl: './carrinho.html',
   styleUrl: './carrinho.css',
 })
 export class Carrinho {
- // A página do carrinho passa a consumir o estado global do carrinho.
-carrinhoFacade = inject(CarrinhoFacade);
-removerItem(indice: number) {
-// Remove um item específico da lista.
-this.carrinhoFacade.removerItem(indice);
+  //! Lógica de carrinho virá nas proximas aulas
+  //! Por ora, apenas exibe a página carrinho
 }
-limparCarrinho() {
-// Limpa todos os itens do carrinho.
-this.carrinhoFacade.limparCarrinho();
-}
-}
-
